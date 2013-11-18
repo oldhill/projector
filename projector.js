e8549592bd884be1f3a8cff5059b6b537c9b7d7b
oldhill <oldhillcode@gmail.com>
2013-11-18 00:24:45 +0000

    get followings as JSON object

--- projector.js
+++ projector.js
@@ -9,16 +9,24 @@ var fs = require('fs');
 var inputs = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 console.log(inputs);
 
-// Sample SoundCloud API request: tracks object for an artist
+// SoundCloud API requests: tracks, followings for an artist
 var api_endpoint = 'http://api.soundcloud.com/users/' + inputs.artistId;
 var tracks_url = api_endpoint + '/tracks.json?client_id=' + inputs.sampleClientId;
+var followings_url = api_endpoint + '/followings.json?client_id=' + inputs.sampleClientId;
 
-http.get(tracks_url, function(res) {
+// Get JSON from the api
+http.get(followings_url, function(res) {
   console.log("Got response: " + res.statusCode);
   console.log("Got headers: " + JSON.stringify(res.headers));
   res.setEncoding('utf8');
+  // Get all teh datas, then concat & parse
+  var bodyData = '';
   res.on('data', function (chunk) {
-    console.log('Got body: ' + chunk);
+    bodyData += chunk;
+  });
+  res.on('end', function() {
+    var bodyObject = JSON.parse(bodyData);
+    console.log(bodyObject);
   });
 }).on('error', function(e) {
   console.log("Error: " + e.message);
